692a148c0ee7d214edbb446c2dd436e5def6859d
Kayshini Jayaraman <Kayshini.AJ@lntinfotech.com>
2020-12-16 09:41:07 +0530

    Updated Project

--- ConsoleAppGitLearn/Program.cs
+++ ConsoleAppGitLearn/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppGitLearn
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Another hello");
         }
     }
 }
